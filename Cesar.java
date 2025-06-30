c1ece0ca08bbefd6efc4dc58e91a4a9941231cf9
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-06-30 13:16:35 +0200

    :bug: fix a bug with cesar

--- Cesar.java
+++ Cesar.java
@@ -3,17 +3,20 @@ public class Cesar {
     public static String encrypt(String text, int key) {
         StringBuilder encryptedText = new StringBuilder();
 
+        int letterKey = key % 26;
+        int digitKey = key % 10;
+
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
 
             if (Character.isLowerCase(c)) {
-                char shifted = (char) ((c - 'a' + key + 26) % 26 + 'a');
+                char shifted = (char) ((c - 'a' + letterKey + 26) % 26 + 'a');
                 encryptedText.append(shifted);
             } else if (Character.isUpperCase(c)) {
-                char shifted = (char) ((c - 'A' + key + 26) % 26 + 'A');
+                char shifted = (char) ((c - 'A' + letterKey + 26) % 26 + 'A');
                 encryptedText.append(shifted);
             } else if (Character.isDigit(c)) {
-                char shifted = (char) ((c - '0' + key + 10) % 10 + '0');
+                char shifted = (char) ((c - '0' + digitKey + 10) % 10 + '0');
                 encryptedText.append(shifted);
             } else {
                 encryptedText.append(c);
